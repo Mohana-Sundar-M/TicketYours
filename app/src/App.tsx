8ff1848e811ad1a19fb6e3fcec29d178f0eda72f
Mohana-Sundar-M <mohanasundar717@gmail.com>
2024-07-28 20:27:34 +0530

    Login,Profile,Theater

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,9 +1,9 @@
-
+//Here you can Place Any Component and See that How It looks 
 import React from 'react';
 /*import Login from './pages/Login';*/
 /*import Nav from './components/login/NavBar';*/
 import Profile from './pages/Profile';
-import Search from './pages/Search'
+/*import Search from './pages/Search'*/
 import { BrowserRouter } from 'react-router-dom';
 
 
--- app/src/pages/Search.tsx
+++ app/src/pages/Search.tsx
@@ -1,3 +1,4 @@
+//Theater Page
 import React from 'react';
 import '../index.css';
 
